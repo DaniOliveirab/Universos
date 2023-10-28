3529f861be4cef870b09aa615f5e182fbd54250b
Danielly Borges <dani.oliveiraab@gmail.com>
2023-10-28 17:27:27 -0300

    Adicionando estilo ao conteudo

--- js/script.js
+++ js/script.js
@@ -8,3 +8,24 @@ function aparecerMenu() {
         document.querySelector(".icon").src = "img/close_white_36dp.svg";
     }
 }
+// Selecione os elementos "a" pelos IDs
+var heartO = document.getElementById("heart-o");
+var heart = document.getElementById("heart");
+
+// Adicione um evento de clique ao ícone "fa fa-heart-o"
+heartO.addEventListener("click", function () {
+    // Alternar entre os ícones "fa fa-heart-o" e "fa fa-heart"
+    heartO.style.display = "none";
+    heart.style.display = "inline";
+});
+
+// Adicione um evento de clique ao ícone "fa fa-heart"
+heart.addEventListener("click", function () {
+    // Alternar entre os ícones "fa fa-heart" e "fa fa-heart-o"
+    heart.style.display = "none";
+    heartO.style.display = "inline";
+});
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+});
