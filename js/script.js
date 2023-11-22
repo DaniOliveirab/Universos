6076be16c0a383bbc3ff177963b9bc49359728f9
Danielly Borges <dani.oliveiraab@gmail.com>
2023-11-22 13:40:34 -0300

    adicionando o background que muda de acordo com o carousel

--- js/script.js
+++ js/script.js
@@ -29,3 +29,17 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems);
 });
+function changeBg(bg, title){
+    const banner = document.querySelector('.banner');
+    const contents = document.querySelectorAll('.content');
+    banner.style.background = `url("img/${bg}"`;
+    banner.style.backgroundSize = 'cover';
+    banner.style.backgroundPosition = 'center';
+
+    contents.forEach(content => {
+        content.classList.remove('active');
+        if(content.classList.contains(title)){
+            content.classList.add('active');
+        }
+    });
+}
